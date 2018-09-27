098083562f442e3b115b85e4646ecfacf815cac5
CSM Tecnologias <juan.9214@gmail.com>
2018-09-26 21:50:48 -0500

    Se implementa el metodo consultar.

--- src/conexionmysqlcorba/Tercero.java
+++ src/conexionmysqlcorba/Tercero.java
@@ -23,7 +23,21 @@ public class Tercero extends TerceroPOA {
 
     @Override
     public String consultarTercero(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String resultado = "";
+        try {
+            String sqlConsultar = "Select * from terceros where id = "+ id;
+            objConec.conectar();
+            Statement st = objConec.conex.createStatement();
+            ResultSet rs = st.executeQuery(sqlConsultar);
+            while(rs.next()){
+                resultado += rs.getString(2) + " - "
+                        + rs.getString(3) + " - "
+                        + rs.getString(4);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: "+ e.getMessage());
+        }
+        return resultado;
     }
 
     @Override
